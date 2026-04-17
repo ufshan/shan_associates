22cf3c4a655d107132509cc8e205b13bccfdbf62
ufshan <98252935+ufshan@users.noreply.github.com>
2026-04-17 22:31:07 +0530

    Add PageLoader component for loading state

---
+++ frontend/src/components/PageLoader.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./PageLoader.css"; // Style as desired
+
+const PageLoader = () => (
+  <div className="page-loader">
+    <div className="spinner" />
+    <p>Loading page...</p>
+  </div>
+);
+
+export default PageLoader;
